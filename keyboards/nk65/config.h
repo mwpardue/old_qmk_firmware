014e80df2e330d19abea190ec036d0730c0ce3d7
Matthew Pardue <mwpardue@gmail.com>
2021-07-31 20:21:36 -0400

    added RGB_MATRIX_RGB_FLUSH_LIMIT 16 and set startup animation to heatmap

--- keyboards/nk65/config.h
+++ keyboards/nk65/config.h
@@ -155,14 +155,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #   define RGB_MATRIX_KEYPRESSES         // reacts to keypresses
 // #define RGB_MATRIX_KEYRELEASES        // reacts to keyreleases (instead of keypresses)
-#   define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_RAINDROPS // Sets the default mode, if none has been set
+#   define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_TYPING_HEATMAP // Sets the default mode, if none has been set
 // #define RGBLIGHT_HUE_STEP 12        // Units to step when in/decreasing hue
 // #define RGBLIGHT_SAT_STEP 25        // Units to step when in/decreasing saturation
 // #define RGBLIGHT_VAL_STEP 12        // Units to step when in/decreasing value (brightness)
 // #define RGB_DISABLE_AFTER_TIMEOUT 0   // number of ticks to wait until disabling effects
 // #define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
 // #define RGB_MATRIX_LED_PROCESS_LIMIT (DRIVER_LED_TOTAL + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-// #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
 // #define RGBLIGHT_ANIMATIONS             // Run RGB animations
 // #define RGBLIGHT_ANIMATIONS             // Enable all additional animation modes.
 // #define RGBLIGHT_EFFECT_ALTERNATING     // Enable alternating animation mode.
