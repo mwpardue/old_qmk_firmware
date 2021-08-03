edc5c016836d5b65f7e7049631d07a97a569c25b
Matthew Pardue <mwpardue@gmail.com>
2021-08-02 22:05:43 -0400

    enabling space cadet shift on nk65/rgbmatrix

--- keyboards/nk65/keymaps/rgbmatrix/rules.mk
+++ keyboards/nk65/keymaps/rgbmatrix/rules.mk
@@ -3,5 +3,6 @@
 #
 RGB_MATRIX_ENABLE = yes            # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE
 RGB_MATRIX_DRIVER = IS31FL3733     # RGB matrix driver
+COMMAND_ENABLE = no                # Enable command functions (disabled for space cadet shift support)
 
 SRC = 
