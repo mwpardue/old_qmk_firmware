4ef306a71ebf6dc62e2e5d5897b412bb21a99a5b
Matthew Pardue <mwpardue@gmail.com>
2021-08-02 20:26:09 -0400

    cleaning up RGB Matrix settings for use with any keymap

--- keyboards/nk65/config.h
+++ keyboards/nk65/config.h
@@ -86,63 +86,63 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BOOTMAGIC_LITE_COLUMN 0
 
 /* Backlight options */
-//#ifdef RGB_BACKLIGHT_ENABLE
-//#define RGB_BACKLIGHT_ENABLED 1
+#ifndef RGB_MATRIX_ENABLE
 
-//#define RGB_BACKLIGHT_NK65
+#define RGB_BACKLIGHT_ENABLED 1
+
+#define RGB_BACKLIGHT_NK65
 
 // they aren't really used if RGB_BACKLIGHT_HS60 defined
-//#define RGB_BACKLIGHT_USE_SPLIT_BACKSPACE 0
-//#define RGB_BACKLIGHT_USE_SPLIT_LEFT_SHIFT 0
-//#define RGB_BACKLIGHT_USE_SPLIT_RIGHT_SHIFT 0
-//#define RGB_BACKLIGHT_USE_7U_SPACEBAR 0
-//#define RGB_BACKLIGHT_USE_ISO_ENTER 0
-//#define RGB_BACKLIGHT_DISABLE_HHKB_BLOCKER_LEDS 0
+#define RGB_BACKLIGHT_USE_SPLIT_BACKSPACE 0
+#define RGB_BACKLIGHT_USE_SPLIT_LEFT_SHIFT 0
+#define RGB_BACKLIGHT_USE_SPLIT_RIGHT_SHIFT 0
+#define RGB_BACKLIGHT_USE_7U_SPACEBAR 0
+#define RGB_BACKLIGHT_USE_ISO_ENTER 0
+#define RGB_BACKLIGHT_DISABLE_HHKB_BLOCKER_LEDS 0
 
 // disable backlight when USB suspended (PC sleep/hibernate/shutdown)
-//#define RGB_BACKLIGHT_DISABLE_WHEN_USB_SUSPENDED 0
+#define RGB_BACKLIGHT_DISABLE_WHEN_USB_SUSPENDED 0
 
 // disable backlight after timeout in minutes, 0 = no timeout
-//#define RGB_BACKLIGHT_DISABLE_AFTER_TIMEOUT 0
+#define RGB_BACKLIGHT_DISABLE_AFTER_TIMEOUT 0
 
 // the default brightness
-//#define RGB_BACKLIGHT_BRIGHTNESS 255
+#define RGB_BACKLIGHT_BRIGHTNESS 255
 
 // the default effect (RGB test)
-//#define RGB_BACKLIGHT_EFFECT 6
+#define RGB_BACKLIGHT_EFFECT 6
 
 // the default effect speed (0-3)
-//#define RGB_BACKLIGHT_EFFECT_SPEED 0
+#define RGB_BACKLIGHT_EFFECT_SPEED 0
 
 // the default color1 and color2
-//#define RGB_BACKLIGHT_COLOR_1 { .h = 0, .s = 255 }
-//#define RGB_BACKLIGHT_COLOR_2 { .h = 127, .s = 255 }
+#define RGB_BACKLIGHT_COLOR_1 { .h = 0, .s = 255 }
+#define RGB_BACKLIGHT_COLOR_2 { .h = 127, .s = 255 }
 
-//#define DRIVER_COUNT 2
-//#define DRIVER_LED_TOTAL 128
+#define DRIVER_COUNT 2
+#define DRIVER_LED_TOTAL 128
 
 // These define which keys in the matrix are alphas/mods
 // Used for backlight effects so colors are different for
 // alphas vs. mods
 // Each value is for a row, bit 0 is column 0
 // Alpha=0 Mod=1
-//#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_0 0b0110000000000001
-//#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_1 0b0100000000000001
-//#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_2 0b0110000000000001
-//#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_3 0b0111000000000001
-//#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_4 0b0111111000000111
+#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_0 0b0110000000000001
+#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_1 0b0100000000000001
+#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_2 0b0110000000000001
+#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_3 0b0111000000000001
+#define RGB_BACKLIGHT_ALPHAS_MODS_ROW_4 0b0111111000000111
 
-//#define RGB_BACKLIGHT_CAPS_LOCK_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
-//#define RGB_BACKLIGHT_LAYER_1_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
-//#define RGB_BACKLIGHT_LAYER_2_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
-//#define RGB_BACKLIGHT_LAYER_3_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
+#define RGB_BACKLIGHT_CAPS_LOCK_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
+#define RGB_BACKLIGHT_LAYER_1_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
+#define RGB_BACKLIGHT_LAYER_2_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
+#define RGB_BACKLIGHT_LAYER_3_INDICATOR { .color = { .h = 0, .s = 0 }, .index = 255 }
 
-//#endif
+// VIA lighting is handled by the keyboard-level code
+#define VIA_CUSTOM_LIGHTING_ENABLE
 
+#else // RGB Matrix Support
 
-// RGB Matrix Support
-
-#ifdef RGB_MATRIX_ENABLE
 #   define DRIVER_ADDR_1 0x50
 #   define DRIVER_ADDR_2 0x52
 #   define DRIVER_COUNT 2
@@ -227,14 +227,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define DISABLE_RGB_MATRIX_MULTISPLASH                   // Full gradient & value pulse away from multiple key hits then fades value out
 #   define DISABLE_RGB_MATRIX_SOLID_SPLASH                  // Hue & value pulse away from a single key hit then fades value out
 #   define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH             // Hue & value pulse away from multiple key hits then fades value out
-#endif
+
+#endif //End of RGB if/else
 
 // Backlight config starts after VIA's EEPROM usage,
 // dynamic keymaps start after this.
 #define VIA_EEPROM_CUSTOM_CONFIG_SIZE 32
-
-// VIA lighting is handled by the keyboard-level code
-//#define VIA_CUSTOM_LIGHTING_ENABLE
 
 /* Custom EEPROM start addressing. This is to support
  * both 128kb and 256kb versions of F303.
---
+++ keyboards/nk65/keymaps/rgbmatrix/rules.mk
@@ -0,0 +1,7 @@
+# Build Options
+#   comment out to disable the options.
+#
+RGB_MATRIX_ENABLE = yes            # Enable keyboard RGB matrix (do not use together with RGBLIGHT_ENABLE
+RGB_MATRIX_DRIVER = IS31FL3733     # RGB matrix driver
+
+SRC = 
